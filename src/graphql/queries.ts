1019f020c85974b62113fe6d3ecd7e062a9a54b2
kanatos <clashofkanat@gmail.com>
2025-08-19 15:31:56 +0500

    graphql query GET_MESSAGES to get a massages, which take first and after parameters, and return message fields and pagination info

---
+++ src/graphql/queries.ts
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client';
+
+export const GET_MESSAGES = gql`
+  query GetMessages($first: Int, $after: MessagesCursor) {
+    messages(first: $first, after: $after) {
+      edges {
+        node {
+          id
+          text
+          status
+          updatedAt
+          sender
+        }
+        cursor
+      }
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+    }
+  }
+`;
